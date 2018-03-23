841e5c8888691cecc1af21a7692379629ec99868
Tech-A12 <tech-a12@Tech-A12s-MacBook-Air.local>
2018-03-23 16:58:05 +0000

    Initial File structure and Initial API call made and puts to console

---
+++ lib/sinatra_api_parser.rb
@@ -0,0 +1,26 @@
+require "json"
+require "httparty"
+
+class SinatraAPIParser
+  attr_reader :api_response
+  include HTTParty
+
+  base_uri ""
+
+  def initialize
+    # Built by LucyBot. www.lucybot.com
+    # https://developer.nytimes.com/top_stories_v2.json#/Console/GET/%7Bsection%7D.%7Bformat%7D
+    uri = URI("https://api.nytimes.com/svc/topstories/v2/home.json")
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    uri.query = URI.encode_www_form({
+      "api-key" => "103a2f8fe48f4435bd2a2bc5244a8931"
+      })
+      request = Net::HTTP::Get.new(uri.request_uri)
+      @api_response = JSON.parse(http.request(request).body)
+      puts @api_response
+  end
+
+end
+
+thing = SinatraAPIParser.new
