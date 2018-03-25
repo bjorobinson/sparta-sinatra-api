448084d7be5e53a312371c36d4c6d8ffd52e03ed
Tech-A12 <tech-a12@android-2da98d21d4cff79c.home>
2018-03-25 14:11:27 +0100

    Filled out expect criteria of tests. Also added boundary tests for indexing

--- spec/sinatra_api_parser_spec.rb
+++ spec/sinatra_api_parser_spec.rb
@@ -25,27 +25,38 @@ describe SinatraAPIParser do
     end
 
     it "should return a list of all article titles" do
-
+      expect(@test_call.get_titles).to be_instance_of Array
     end
 
-    it "should, using article title, return its index in results" do
+    it "should return the maximum index of results" do
+      expect(@test_call.get_max_index).to be_instance of Integer
+    end
 
+    it "should check if a number provided as an arguement is within the valid bounds of the index range" do
+      expect(@test_call.is_valid_index(0)).to be true
+      expect(@test_call.is_valid_index(@test_call.get_max_index)).to be true
+      expect(@test_call.is_valid_index(-1).to be false
+      expect(@test_call.is_valid_index(@test_call.get_max_index)+1).to be false
+    end
+
+    it "should, using an index in results, return articles title" do
+      expect(@test_call.get_title(0)).to be_instance_of String
     end
 
     it "should, using an article index, return the articles section" do
-
+      expect(@test_call.get_section(0)).to be_instance_of String
     end
 
     it "should, using an article index, return the articles abstract" do
-
+      expect(@test_call.get_abstract(0)).to be_instance_of String
     end
 
     it "should, using an article index, return the articles shortenned URL" do
-
+      expect(@test_call.get_url(0)).to be_instance_of String
     end
 
     it "should, using an article index, return the articles byline" do
-      
+      expect(@test_call.get_byline(0)).to be_instance_of String
     end
   end
 end
